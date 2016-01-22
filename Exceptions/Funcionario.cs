f073a18d95d0f1b52223c850b382fd92aa4b69b0
Ale DP <dpsantos@gmail.com>
2016-01-22 10:34:06 -0200

    Exmplo

---
+++ Exceptions/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+	class Funcionario
+	{
+		private double salario;
+
+		public void AumentaSalario(double aumento)
+		{
+			if (aumento < 0)
+			{
+				ArgumentException erro = new ArgumentException("Valor negativo, não permitido.");
+				throw erro;
+			}
+			else
+			{
+				this.salario += aumento;
+			}
+		}
+
+	}
+}
---
+++ Exceptions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Funcionario f = new Funcionario();
+
+			try
+			{
+				f.AumentaSalario(-10);
+			}
+			catch (ArgumentException a)
+			{
+
+				Console.WriteLine("Error : {0}", a.Message);
+			}
+		}
+	}
+}
